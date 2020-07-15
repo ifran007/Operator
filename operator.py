c7b9e7fd8868b1c6b99058de0c7e0f890eb41138
Mohammad Ifran Uddin <67389212+ifran007@users.noreply.github.com>
2020-07-15 22:42:13 +0600

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,38 @@
+#Addition (=)
+a = 58
+b = 91
+c = a+b
+print("\n Addition Value is :", c )
+#Subtraction (-)
+a = 72
+b = 8
+c = a-b
+print("\n Subtraction  is :", c)
+#Multiplication (*)
+a = 784
+b = 205
+c = a*b
+print("\n Multiplication Value is :", c)
+#Division (/)
+a = 45
+b = 4
+c = a/b
+print("\n Division value is :", c )
+#Modulus (%)
+
+a = 49
+b = 2
+c = a%b
+print("\n Modulus value is:", c)
+#Esponentiation (**)
+a = 2
+b = 3
+c = a**b
+print("\n Esponentiation value is :", c , " (same as 2*2*2*)")
+#Floor_division (//)
+a = 21
+b = 5
+c = a//b
+print("\n Floor division value is:", c)
+
+# Ifran Uddin = PS H:\PythonBeginner> & C:/Users/ifran/AppData/Local/Programs/Python/Python38-32/python.exe h:/PythonBeginner/operator.py
